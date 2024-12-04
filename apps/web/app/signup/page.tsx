472bd25f52b080912d81d1a0dd3ca525e2d02a35
deucenn <panix.black@web.de>
2024-12-04 20:49:00 +0100

    Built setup

---
+++ apps/web/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignUp() {
+  return (
+    <div className="font-sans border size-96">
+      <h1>Sign Up</h1>
+      <p>Create a new account</p>
+      {/* Sign up form */}
+    </div>
+  );
+}
