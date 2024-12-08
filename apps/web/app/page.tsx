ff42b9ecb4b08061b1fe964874eb61e71e3215ae
deucenn <panix.black@web.de>
2024-12-08 20:11:22 +0100

    Initalized tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
 
 export default function Home() {
-  return <div className="size-56">Hello</div>;
+  return <div className="size-56 mt-12">Hello</div>;
 }
--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
   return (
-    <div className="font-sans border size-96">
+    <div className="font-sans">
       <h1>Sign Up</h1>
       <p>Create a new account</p>
       {/* Sign up form */}
