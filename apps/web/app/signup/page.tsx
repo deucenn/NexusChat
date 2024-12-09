ffbc953b1fd42e2bdb6efbf015cfc7a0b1224d85
deucenn <panix.black@web.de>
2024-12-09 21:45:20 +0100

    Building signup page

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,8 +1,12 @@
 export default function SignUp() {
   return (
-    <div className="font-sans">
-      <h1>Sign Up</h1>
-      <p>Create a new account</p>
+    <div className="font-sans md:grid-rows-2">
+      <div>
+        <h1>Sign Up</h1>
+      </div>
+      <div>
+        <p>Create a new account</p>
+      </div>
       {/* Sign up form */}
     </div>
   );
